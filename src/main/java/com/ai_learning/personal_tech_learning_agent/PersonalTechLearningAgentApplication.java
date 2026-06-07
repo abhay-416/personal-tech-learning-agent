8b86b756f5ba64fcd84e6701f0132a19e9bde805
Abhay Upadhayay <abhayupadhyay63@gmail.com>
2026-06-07 12:37:19 +0530

    Initial commit with clean Spring Boot structure

---
+++ src/main/java/com/ai_learning/personal_tech_learning_agent/PersonalTechLearningAgentApplication.java
@@ -0,0 +1,13 @@
+package com.ai_learning.personal_tech_learning_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTechLearningAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalTechLearningAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai_learning/personal_tech_learning_agent/PersonalTechLearningAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai_learning.personal_tech_learning_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalTechLearningAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
